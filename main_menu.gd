6c500c78cc21256678ef5370f3148decc3113798
jonnali921-boop <jonnali921@gmail.com>
2026-06-30 19:05:41 -0400

    added settings, modified main menu
    also changed custom textbox back to normal

--- main_menu.gd
+++ main_menu.gd
@@ -1,23 +1,32 @@
 extends Control
 
+@onready var main_buttons: VBoxContainer = $MainButtons
+@onready var settings: Panel = $Settings
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	main_buttons.visible = true
+	settings.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
+	
+	
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/desktop.tscn")
 
 
 func _on_settings_pressed() -> void:
 	print("Settings pressed")
+	main_buttons.visible = false
+	settings.visible = true
 
 
 func _on_leave_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_exit_pressed() -> void:
+	_ready()
--- scenes/start_script.gd
+++ scenes/start_script.gd
@@ -4,7 +4,7 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	await get_tree().process_frame
-	Dialogic.start("res://timeline/kai_gets_drunk.dtl")
+	Dialogic.start("res://timeline/scraps.dtl")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
